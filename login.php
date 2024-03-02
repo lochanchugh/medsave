13c5a49907fd8f4c5fb7fc20039610c327b6ea92
Lochan <98405764+lochanchugh@users.noreply.github.com>
2024-03-02 21:17:33 +0530

    login page done

--- login.php
+++ login.php
@@ -108,5 +108,71 @@
     }
 
     ?>
+<center>
+    <div class="container">
+        <table border="0" style="margin: 0;padding: 0;width: 60%;">
+            <tr>
+                <td>
+                    <p class="header-text">Welcome Back!</p>
+                </td>
+            </tr>
+        <div class="form-body">
+            <tr>
+                <td>
+                    <p class="sub-text">Login with your details to continue</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td">
+                    <label for="useremail" class="form-label">Email: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <input type="email" name="useremail" class="input-text" placeholder="Email Address" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <label for="userpassword" class="form-label">Password: </label>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="label-td">
+                    <input type="Password" name="userpassword" class="input-text" placeholder="Password" required>
+                </td>
+            </tr>
+
+
+            <tr>
+                <td><br>
+                <?php echo $error ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type="submit" value="Login" class="login-btn btn-primary btn">
+                </td>
+            </tr>
+        </div>
+            <tr>
+                <td>
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Don't have an account&#63; </label>
+                    <a href="signup.php" class="hover-link1 non-style-link">Sign Up</a>
+                    <br><br><br>
+                </td>
+            </tr>
+                        
+                        
+    
+                        
+                    </form>
+        </table>
+
+    </div>
 </body>
 </html>
