604999a25fc484b02fa232e15c4e26a6152d373c
Lochan <98405764+lochanchugh@users.noreply.github.com>
2024-03-02 21:15:58 +0530

    signup page created

---
+++ signup.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">  
+    <link rel="stylesheet" href="css/main.css">  
+    <link rel="stylesheet" href="css/signup.css">
+        
+    <title>Sign Up</title>
+    
+</head>
+<body>
+<?php
+
+
+session_start();
+
+$_SESSION["user"]="";
+$_SESSION["usertype"]="";
+
+// Set the new timezone
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+
+$_SESSION["date"]=$date;
+
+
+
+if($_POST){
+
+    
+
+    $_SESSION["personal"]=array(
+        'fname'=>$_POST['fname'],
+        'lname'=>$_POST['lname'],
+        'address'=>$_POST['address'],
+        'nic'=>$_POST['nic'],
+        'dob'=>$_POST['dob']
+    );
+
+
+    print_r($_SESSION["personal"]);
+    header("location: create-account.php");
+
+
+
+
+}
+
+?>
+</body>
+</html>
