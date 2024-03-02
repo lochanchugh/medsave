863a3b050b78c02c30f9418276dcd6ed2cf9f765
NegiSenpai <117892538+Abhaynegi1@users.noreply.github.com>
2024-03-02 21:30:54 +0530

    sign up page done

--- signup.php
+++ signup.php
@@ -50,5 +50,87 @@ if($_POST){
 }
 
 ?>
+    <center>
+    <div class="container">
+        <table border="0">
+            <tr>
+                <td colspan="2">
+                    <p class="header-text">Let's Get Started</p>
+                    <p class="sub-text">Add Your Personal Details to Continue</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td" colspan="2">
+                    <label for="name" class="form-label">Name: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <input type="text" name="fname" class="input-text" placeholder="First Name" required>
+                </td>
+                <td class="label-td">
+                    <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="address" class="form-label">Address: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="address" class="input-text" placeholder="Address" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="nic" class="form-label">NIC: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="nic" class="input-text" placeholder="NIC Number" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="dob" class="form-label">Date of Birth: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="date" name="dob" class="input-text" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
+                </td>
+                <td>
+                    <input type="submit" value="Next" class="login-btn btn-primary btn">
+                </td>
+
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
+                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                    <br><br><br>
+                </td>
+            </tr>
+
+                    </form>
+            </tr>
+        </table>
+
+    </div>
+</center>
 </body>
 </html>
